f7d05f18f817cc2cad6c043b098014ae8f46926f
HoualefAkram <akram1232004@gmail.com>
2025-06-29 17:35:47 +0100

    UI performance improvements

--- lib/features/auth/views/login_view.dart
+++ lib/features/auth/views/login_view.dart
@@ -56,86 +56,73 @@ class _LoginViewState extends State<LoginView> {
         }
       },
       child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: SingleChildScrollView(
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height,
-            ),
-            child: IntrinsicHeight(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 120),
-                    CustomIcon.build(CIcon.messenger, height: 80, width: 80),
-                    SizedBox(height: 120),
-                    TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        hint: Text("Email", style: TextStyleManager.hintText),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    TextField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hint: Text(
-                          "Password",
-                          style: TextStyleManager.hintText,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    SizedBox(
-                      width: MediaQuery.sizeOf(context).width,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          context.read<AuthBloc>().add(
-                            AuthLoginEvent(
-                              email: emailController.text,
-                              password: passwordController.text,
-                            ),
-                          );
-                        },
-                        child: Text("Log in", style: TextStyleManager.large()),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Forgot password?",
-                        style: TextStyleManager.large(),
-                      ),
-                    ),
-                    Spacer(),
-                    SizedBox(
-                      width: MediaQuery.sizeOf(context).width,
-                      height: 50,
-                      child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed(Routes.register);
-                        },
-                        child: const Text("Create new account"),
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    Text(
-                      "Houalef Akram",
-                      style: TextStyleManager.large(
-                        bold: true,
-                        color: Colors.grey.withAlpha(90),
-                      ),
-                    ),
-                  ],
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 120),
+              CustomIcon.build(CIcon.messenger, height: 80, width: 80),
+              SizedBox(height: 120),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  hint: Text("Email", style: TextStyleManager.hintText),
                 ),
               ),
-            ),
+              SizedBox(height: 12),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hint: Text("Password", style: TextStyleManager.hintText),
+                ),
+              ),
+              SizedBox(height: 12),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(
+                      AuthLoginEvent(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      ),
+                    );
+                  },
+                  child: Text("Log in", style: TextStyleManager.large()),
+                ),
+              ),
+              SizedBox(height: 12),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Forgot password?",
+                  style: TextStyleManager.large(),
+                ),
+              ),
+              Spacer(),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                height: 50,
+                child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(Routes.register);
+                  },
+                  child: const Text("Create new account"),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                "Houalef Akram",
+                style: TextStyleManager.large(
+                  bold: true,
+                  color: Colors.grey.withAlpha(90),
+                ),
+              ),
+            ],
           ),
         ),
       ),
