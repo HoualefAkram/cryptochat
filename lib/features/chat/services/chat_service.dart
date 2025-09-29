ac61d5347c7702f184bec3ae85bdac386f657ab0
HoualefAkram <akram1232004@gmail.com>
2025-09-29 22:36:23 +0100

    added broadcast sockets

--- lib/features/chat/services/chat_service.dart
+++ lib/features/chat/services/chat_service.dart
@@ -1,3 +1,6 @@
+import 'dart:developer' show log;
+import 'dart:io';
+
 import 'package:cryptochat/features/auth/models/auth_user.dart';
 import 'package:cryptochat/features/chat/models/message.dart';
 import 'package:cryptochat/features/shared/services/db/db_service.dart';
@@ -17,4 +20,34 @@ class ChatService {
   static Future<List<Message>> getAllMesages() => _db.getAllMesages();
 
   static Stream<Iterable<Message>> getMessageStream() => _db.getMessageStream();
+
+  Future<void> udpBroadcast() async {
+    final rawSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+    rawSocket.broadcastEnabled = true;
+
+    final message = "Hello everyone on the network!";
+    final data = message.codeUnits;
+
+    rawSocket.send(data, InternetAddress("255.255.255.255"), 4040);
+
+    log("Broadcast sent: $message");
+  }
+
+  Future<void> udpListener() async {
+    final rawSocket = await RawDatagramSocket.bind(
+      InternetAddress.anyIPv4,
+      4040,
+    );
+    log("Listening for UDP broadcasts on port 4040...");
+
+    rawSocket.listen((event) {
+      if (event == RawSocketEvent.read) {
+        final datagram = rawSocket.receive();
+        if (datagram != null) {
+          final message = String.fromCharCodes(datagram.data);
+          log("Received from ${datagram.address.address}: $message");
+        }
+      }
+    });
+  }
 }
